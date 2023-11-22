35f98170ff03fd39439c63a9980053984ab30ed7
PeritxCreate <143961072+PeritxCreate@users.noreply.github.com>
2023-11-22 20:19:19 +0000

    estructura react acabada

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Footer = () => {
+
+	return (
+		
+		<div className="text-center bg-secondary h-25">
+			copyright
+		</div>
+		
+	);
+};
+
+export default Footer;
---
+++ src/js/component/header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+const Header = () => {
+
+	return (
+		
+		<div>
+			<nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                 <span class="navbar-brand mb-0 h1">HEADER!</span>
+                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas, ut doloremque. Laboriosam, iure alias quae velit nesciunt expedita enim animi maxime explicabo ad placeat perspiciatis a quisquam eveniet ut consectetur? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat nam, quam pariatur harum molestias ad illum magni, maxime delectus id unde dolorum adipisci, sunt voluptatem eaque. Error quisquam sit illum!</p>
+                </div>
+            </nav>
+		</div>
+		
+	);
+};
+
+export default Header;
